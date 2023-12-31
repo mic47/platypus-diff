06b699a8667c8ddf3b8018b21f006d214314d6e4
Michal Nanasi <michal@merlon.ai>
2023-12-31 12:43:49 +0100

    Add edit distance alignment algo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, rc::Rc};
 
 use clap::Parser;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 enum TokenType {
     WhiteSpace,
     SpecialCharacter,
     Word,
 }
 
+#[derive(Clone)]
 struct Token<'a, T> {
     source: &'a str,
     start: usize,
@@ -20,11 +21,23 @@ struct Token<'a, T> {
 impl<'a, T: std::fmt::Debug> std::fmt::Debug for Token<'a, T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Token")
-            .field("text", &self.source.get(self.start..self.end).unwrap())
+            .field("text", &self.text())
             .field("start", &self.start)
             .field("end", &self.end)
             .field("t", &self.t)
             .finish()
+    }
+}
+
+impl<'a, T> Token<'a, T> {
+    pub fn text(&self) -> &'a str {
+        self.source.get(self.start..self.end).unwrap()
+    }
+}
+
+impl<'a, T: PartialEq> PartialEq for Token<'a, T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.t == other.t && self.text() == other.text()
     }
 }
 
@@ -89,16 +102,158 @@ impl<'a> Iterator for TokenParser<'a> {
 // TODO: Eventually better parsing -- i.e. add BlockStart/BlockEnd for non-whitesace things
 // TODO: Add line and col numbers to tokens
 
+#[derive(Debug, Clone)]
+enum AlignmentOperation<T> {
+    Mutation { left: T, right: T },
+    InsertLeft { left: T },
+    InsertRight { right: T },
+}
+
+#[derive(Debug)]
+enum AlignmentOperationType {
+    Mutation,
+    InsertLeft,
+    InsertRight,
+}
+
+#[derive(Debug, Clone)]
+enum PathList<T> {
+    End,
+    Node {
+        payload: T,
+        previous: Rc<PathList<T>>,
+    },
+}
+
+impl<T: Clone> PathList<T> {
+    pub fn extract_path<'a>(self: Self) -> Vec<T> {
+        let mut out = vec![];
+        let mut current = self;
+        loop {
+            current = match current {
+                PathList::End => break,
+                PathList::Node { payload, previous } => {
+                    out.push(payload);
+                    Rc::try_unwrap(previous).unwrap_or_else(|x| {
+                        eprintln!("More than 1 reference!");
+                        (*x).clone()
+                    })
+                }
+            }
+        }
+        out.reverse();
+        out
+    }
+}
+
+fn align<'a>(
+    left: &'a [Token<'a, TokenType>],
+    right: &'a [Token<'a, TokenType>],
+) -> Vec<AlignmentOperation<&'a Token<'a, TokenType>>> {
+    let result_path = {
+        let mut current: Vec<(
+            f64,
+            Rc<PathList<AlignmentOperation<&'a Token<'a, TokenType>>>>,
+        )> = Vec::with_capacity(left.len() + 1);
+        current.push((0.0, Rc::new(PathList::End)));
+        for l in left.iter() {
+            let prev = current.last().unwrap();
+            current.push((
+                prev.0 + 1.,
+                Rc::new(PathList::Node {
+                    payload: AlignmentOperation::InsertLeft { left: l },
+                    previous: prev.1.clone(),
+                }),
+            ))
+        }
+        let mut next = Vec::with_capacity(left.len() + 1);
+        for r in right.iter() {
+            let prev = &current[0];
+            next.push((
+                prev.0 + 1.,
+                Rc::new(PathList::Node {
+                    payload: AlignmentOperation::InsertRight { right: r },
+                    previous: prev.1.clone(),
+                }),
+            ));
+            for (l_index, l) in left.iter().enumerate() {
+                let l_index = l_index + 1;
+                let insert_right = (
+                    current[l_index].0 + 1.,
+                    &current[l_index].1,
+                    AlignmentOperationType::InsertRight,
+                );
+                let prev = next.last().unwrap();
+                let insert_left = (prev.0 + 1., &prev.1, AlignmentOperationType::InsertLeft);
+                let diag = &current[l_index - 1];
+                let mutation = (
+                    diag.0 + if l == r { 0. } else { 1. },
+                    &diag.1,
+                    AlignmentOperationType::Mutation,
+                );
+                let best = if mutation.0 < insert_right.0 {
+                    if mutation.0 < insert_left.0 {
+                        mutation
+                    } else {
+                        insert_left
+                    }
+                } else {
+                    if insert_right.0 < insert_left.0 {
+                        insert_right
+                    } else {
+                        insert_left
+                    }
+                };
+                next.push((
+                    best.0,
+                    Rc::new(PathList::Node {
+                        payload: match best.2 {
+                            AlignmentOperationType::Mutation => {
+                                AlignmentOperation::Mutation { left: l, right: r }
+                            }
+                            AlignmentOperationType::InsertLeft => {
+                                AlignmentOperation::InsertLeft { left: l }
+                            }
+                            AlignmentOperationType::InsertRight => {
+                                AlignmentOperation::InsertRight { right: r }
+                            }
+                        },
+                        previous: best.1.clone(),
+                    }),
+                ));
+            }
+
+            std::mem::swap(&mut current, &mut next);
+            next.clear()
+        }
+        current.pop().unwrap().1
+    };
+    Rc::try_unwrap(result_path)
+        .unwrap_or_else(|x| {
+            eprintln!("More than 1 reference!");
+            (*x).clone()
+        })
+        .extract_path()
+}
+
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    file: PathBuf,
+    left: PathBuf,
+    right: PathBuf,
 }
 
 fn main() {
     let cli = Cli::parse();
-    let text = std::fs::read_to_string(cli.file).unwrap();
-    for token in TokenParser::parse(&text) {
-        println!("{:?}", token)
+    let left_text = std::fs::read_to_string(cli.left).unwrap();
+    let right_text = std::fs::read_to_string(cli.right).unwrap();
+    let left_tokens = TokenParser::parse(&left_text)
+        .filter(|x| x.t != TokenType::WhiteSpace)
+        .collect::<Vec<_>>();
+    let right_tokens = TokenParser::parse(&right_text)
+        .filter(|x| x.t != TokenType::WhiteSpace)
+        .collect::<Vec<_>>();
+    for operation in align(&left_tokens, &right_tokens).into_iter() {
+        println!("{:?}", operation)
     }
 }
