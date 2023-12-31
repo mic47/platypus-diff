96ac883fb841916657e2a70aec60f5cb580cb092
Michal Nanasi <michal@merlon.ai>
2023-12-31 15:47:55 +0100

    Add whitespace tokens back

--- src/main.rs
+++ src/main.rs
@@ -149,7 +149,7 @@ impl<T: Clone> PathList<T> {
 fn align<'a>(
     left: &'a [Token<'a, TokenType>],
     right: &'a [Token<'a, TokenType>],
-) -> Vec<AlignmentOperation<&'a Token<'a, TokenType>>> {
+) -> Alignment<'a, Token<'a, TokenType>> {
     let result_path = {
         let mut current: Vec<(
             f64,
@@ -228,12 +228,89 @@ fn align<'a>(
         }
         current.pop().unwrap().1
     };
-    Rc::try_unwrap(result_path)
-        .unwrap_or_else(|x| {
-            eprintln!("More than 1 reference!");
-            (*x).clone()
-        })
-        .extract_path()
+    Alignment {
+        left_tokens: left,
+        right_tokens: right,
+        alignment: Rc::try_unwrap(result_path)
+            .unwrap_or_else(|x| {
+                eprintln!("More than 1 reference!");
+                (*x).clone()
+            })
+            .extract_path(),
+    }
+}
+
+pub struct Alignment<'a, T> {
+    left_tokens: &'a [T],
+    right_tokens: &'a [T],
+    alignment: Vec<AlignmentOperation<&'a T>>,
+}
+
+impl<T> AlignmentOperation<T> {
+    pub fn left(&self) -> Option<&T> {
+        match self {
+            AlignmentOperation::Mutation { left, right: _ } => Some(left),
+            AlignmentOperation::InsertLeft { left } => Some(left),
+            AlignmentOperation::InsertRight { right: _ } => None,
+        }
+    }
+    pub fn right(&self) -> Option<&T> {
+        match self {
+            AlignmentOperation::Mutation { left: _, right } => Some(right),
+            AlignmentOperation::InsertLeft { left: _ } => None,
+            AlignmentOperation::InsertRight { right } => Some(right),
+        }
+    }
+}
+
+impl<'a> Alignment<'a, Token<'a, TokenType>> {
+
+    pub fn add_tokens(
+        &mut self,
+        left: &'a[Token<'a, TokenType>],
+        right: &'a[Token<'a, TokenType>],
+    ) {
+        let mut old_alignment = Vec::with_capacity(self.alignment.len() + left.len() + right.len());
+        std::mem::swap(&mut old_alignment, &mut self.alignment);
+        let mut left = left.iter().peekable();
+        let mut right = right.iter().peekable();
+        let mut left_position = None;
+        let mut right_position = None;
+        old_alignment.reverse();
+        while let Some(a) = old_alignment.pop() {
+            left_position = a.left().cloned().or(left_position);
+            if let Some(left_position) = left_position {
+                while left
+                    .peek()
+                    .map(|p| p.start < left_position.start)
+                    .unwrap_or(false)
+                {
+                    left.next().map(|left| {
+                        self.alignment
+                            .push(AlignmentOperation::InsertLeft { left: &left })
+                    });
+                }
+            }
+            right_position = a.right().cloned().or(right_position);
+            if let Some(right_position) = right_position {
+                while right
+                    .peek()
+                    .map(|p| p.start < right_position.start)
+                    .unwrap_or(false)
+                {
+                    right.next().map(|right| {
+                        self.alignment
+                            .push(AlignmentOperation::InsertRight { right: &right })
+                    });
+                }
+            }
+            self.alignment.push(a);
+        }
+        self.alignment
+            .extend(left.map(|left| AlignmentOperation::InsertLeft { left }));
+        self.alignment
+            .extend(right.map(|right| AlignmentOperation::InsertRight { right }));
+    }
 }
 
 #[derive(Parser)]
@@ -247,13 +324,14 @@ fn main() {
     let cli = Cli::parse();
     let left_text = std::fs::read_to_string(cli.left).unwrap();
     let right_text = std::fs::read_to_string(cli.right).unwrap();
-    let left_tokens = TokenParser::parse(&left_text)
-        .filter(|x| x.t != TokenType::WhiteSpace)
-        .collect::<Vec<_>>();
-    let right_tokens = TokenParser::parse(&right_text)
-        .filter(|x| x.t != TokenType::WhiteSpace)
-        .collect::<Vec<_>>();
-    for operation in align(&left_tokens, &right_tokens).into_iter() {
+    let (left_tokens, left_whitespaces): (Vec<_>, Vec<_>) =
+        TokenParser::parse(&left_text).partition(|x| x.t != TokenType::WhiteSpace);
+    let (right_tokens, right_whitespaces): (Vec<_>, Vec<_>) =
+        TokenParser::parse(&right_text).partition(|x| x.t != TokenType::WhiteSpace);
+    // TODO: removal of whitespace tokens should be implementation detail of align?
+    let mut alignment = align(&left_tokens, &right_tokens);
+    alignment.add_tokens(&left_whitespaces, &right_whitespaces);
+    for operation in alignment.alignment.into_iter() {
         println!("{:?}", operation)
     }
 }
